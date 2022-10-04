5c1d33e56e06566c9f5a78189558357a1a71bbd3
Boronkin Egor <113104407+EgorBoronkin@users.noreply.github.com>
2022-10-04 11:08:44 +0300

    Fix

--- Source.cpp
+++ Source.cpp
@@ -1,35 +1,33 @@
 #include<stdio.h>
-#include<locale.h>
 void main()
 {
-	setlocale(LC_ALL, "Russian");
 	int h, w, d;
-	float  p_dvp, p_dsp, p_wood, m;
+	float  p_dvp, p_dsp, p_wood, weight;
 	p_dsp = 0.6;
 	p_dvp = 0.8;
 	p_wood = 0.5;
-	printf("Плотность двп - %f г/cм в кубе, плотность дсп - %f г/cм в кубе, плотность дерева - %f г/cм в кубе \n", p_dvp, p_dsp, p_wood);
-	printf("Введите высоту от 180 см до 220 см:");
+	printf("Density of dsp - %f g/cm cubed, density of dvp - %f g/cm cubed, density of wood - %f g/cm cubed \n", p_dsp, p_dvp, p_wood);
+	printf("Write height from 180 cm to 220 cm:");
 	scanf_s("%i", &h);
-	printf("Введите ширину от 80 см до 120 см:");
+	printf("Write height from 80 cm to 120 cm:");
 	scanf_s("%i", &w);
-	printf("Введите глубину от 50 см до 90 см:");
+	printf("Write height from 50 cm to 90 cm:");
 	scanf_s("%i", &d);
-	if ((180 <= h  && h<= 220) && (80 <= w && w <= 120) && (50 <= d && d <= 90))
+	if ((180 <= h && h <= 220) && (80 <= w && w <= 120) && (50 <= d && d <= 90))
 	{
-		if (160 <= h < 205)
+		if (h < 220)
 		{
-			m = ((h * w * 0.5 * p_dvp) + (h * d * 1.5 * p_dsp * 2) + (w * d * 1.5 * p_dsp * 2) + (h * w * p_wood * 2) + (w * d * 15 * p_dsp * 2))/1000;
-			printf("Масса шкафа - %.2f кг", m);
+			weight = ((h * w * 0.5 * p_dvp) + (h * d * 1.5 * p_dsp * 2) + (w * d * 1.5 * p_dsp * 2) + (h * w * p_wood * 2) + (w * d * 1.5 * p_dsp * 4)) / 1000;
+			printf("Weight of wardrobe - %.2f kg", weight);
 		}
-		else 
+		else
 		{
-			m = ((h * w * 0.5 * p_dvp) + (h * d * 1.5 * p_dsp * 2) + (w * d * 1.5 * p_dsp * 2) + (h * w * p_wood * 2) + (w * d * 15 * p_dsp * 3))/1000;
-			printf("Масса шкафа - %.2f кг", m);
+			weight = ((h * w * 0.5 * p_dvp) + (h * d * 1.5 * p_dsp * 2) + (w * d * 1.5 * p_dsp * 2) + (h * w * p_wood * 2) + (w * d * 1.5 * p_dsp * 5)) / 1000;
+			printf("Weight of wardrobe - %.2f kg", weight);
 		}
 	}
-	else 
-	{ 
-		printf("Данные введены некоректно");
+	else
+	{
+		printf("Wrong data");
 	}
 }
